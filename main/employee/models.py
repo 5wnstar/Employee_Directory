50e6d99f41619c59070b6b8e7ad8cb40b8eb3ccf
Toheeb Kayode Adisa <pwnstar@pwnstar-sec.idan>
2024-05-29 15:54:27 +0100

    first commit

---
+++ main/employee/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here
+class Employee(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name =  models.CharField(max_length=250)
+    position =  models.CharField(max_length=250)
+    job_type =  models.CharField(max_length=250)
+    image = models.ImageField(upload_to='employee')
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name
